ff69bf0f385b804d1af168c0ff4cf051041bf5f3
Dksie09 <dakshiegoel@gmail.com>
2024-07-12 11:39:43 +0530

    alignment fixes

--- app/components/ComponentGrid.tsx
+++ app/components/ComponentGrid.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-
 import {
   Button,
   Input,
@@ -10,7 +9,6 @@ import {
   Popup,
 } from "pixel-retroui";
 import { ThemeOptions, themeColors } from "../themes";
-import CopyableCode from "./CopyableCode";
 import { useRouter } from "next/navigation";
 
 interface ComponentGridProps {
@@ -121,7 +119,7 @@ const ComponentGrid: React.FC<ComponentGridProps> = ({ theme }) => {
       case "TextArea":
         return (
           <Component
-            className="w-full max-w-xs h-24"
+            className="w-full max-w-xs"
             bg={colors.textareaBg}
             textColor={colors.text}
             borderColor={colors.border}
@@ -132,24 +130,31 @@ const ComponentGrid: React.FC<ComponentGridProps> = ({ theme }) => {
       case "Popup":
         return (
           <div
-            className="relative w-full max-w-xs h-36 border-4 border-dashed"
+            className="relative w-full max-w-xs h-36 border-4 border-dashed cursor-pointer overflow-hidden"
             style={{ borderColor: colors.border }}
+            onClick={(e) => {
+              e.stopPropagation();
+              setSelectedComponent(name);
+              router.push(`/${name}`);
+            }}
           >
-            <div className="absolute top-1/2 -left-8 transform -translate-x-1/2 -translate-y-1/2">
-              <Component
-                isOpen={true}
-                onClose={() => {}}
-                bg={colors.bg}
-                textColor={colors.text}
-                borderColor={colors.border}
-                baseBg={colors.shadow}
-                className=" text-center w-96"
-              >
-                <p className="">
-                  Popup content goes
-                  <br /> here
-                </p>
-              </Component>
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="relative w-full h-full flex items-center justify-center">
+                <Component
+                  isOpen={true}
+                  onClose={() => {}}
+                  bg={colors.bg}
+                  textColor={colors.text}
+                  borderColor={colors.border}
+                  baseBg={colors.shadow}
+                  className="z-0 text-center w-48 pointer-events-none scale-75 transform !absolute !left-1/2 !top-1/2 !-translate-x-1/2 !-translate-y-1/2"
+                >
+                  <p className="">
+                    Popup content goes
+                    <br /> here
+                  </p>
+                </Component>
+              </div>
             </div>
           </div>
         );
@@ -159,327 +164,46 @@ const ComponentGrid: React.FC<ComponentGridProps> = ({ theme }) => {
     }
   };
 
-  // const renderDetailedView = (name: string, Component: any) => {
-  //   const getPropsTable = () => {
-  //     // This is a placeholder. You should replace this with actual prop documentation for each component.
-  //     const propsDocs = {
-  //       Button: [
-  //         {
-  //           name: "bg",
-  //           type: "string",
-  //           default: "white",
-  //           description: "Background color of the button",
-  //         },
-  //         {
-  //           name: "textColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Text color of the button",
-  //         },
-  //         {
-  //           name: "borderColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Border color of the button",
-  //         },
-  //         {
-  //           name: "shadow",
-  //           type: "string",
-  //           default: "gray",
-  //           description: "Shadow color of the button",
-  //         },
-  //         {
-  //           name: "className",
-  //           type: "string",
-  //           default: "",
-  //           description: "Additional tailwind + CSS classes",
-  //         },
-  //       ],
-  //       Input: [
-  //         {
-  //           name: "bg",
-  //           type: "string",
-  //           default: "white",
-  //           description: "Background color of the input",
-  //         },
-  //         {
-  //           name: "textColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Text color of the input",
-  //         },
-  //         {
-  //           name: "borderColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Border color of the input",
-  //         },
-  //         {
-  //           name: "icon",
-  //           type: "string",
-  //           default: "",
-  //           description: "Icon on the right of the input",
-  //         },
-  //         {
-  //           name: "placeholder",
-  //           type: "string",
-  //           default: "",
-  //           description: "Placeholder text",
-  //         },
-  //         {
-  //           name: "className",
-  //           type: "string",
-  //           default: "",
-  //           description: "Additional tailwind + CSS classes",
-  //         },
-  //       ],
-  //       Card: [
-  //         {
-  //           name: "bg",
-  //           type: "string",
-  //           default: "white",
-  //           description: "Background color of the input",
-  //         },
-  //         {
-  //           name: "textColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Text color of the input",
-  //         },
-  //         {
-  //           name: "borderColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Border color of the input",
-  //         },
-  //         {
-  //           name: "shadow",
-  //           type: "string",
-  //           default: "gray",
-  //           description: "Shadow color of the input",
-  //         },
-  //         {
-  //           name: "className",
-  //           type: "string",
-  //           default: "",
-  //           description: "Additional tailwind + CSS classes",
-  //         },
-  //       ],
-  //       ProgressBar: [
-  //         {
-  //           name: "progress",
-  //           type: "number",
-  //           default: "",
-  //           description: "Progress in percentage of the progress bar",
-  //         },
-  //         {
-  //           name: "color",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Color of the progress bar",
-  //         },
-  //         {
-  //           name: "borderColor",
-  //           type: "string",
-  //           default: "black",
-  //           description: "Border color of the progress bar",
-  //         },
-  //         {
-  //           name: "size",
-  //           type: '"md" | "sm" | "lg"',
-  //           default: "md",
-  //           description: "Size of the progress bar",
-  //         },
-  //         {
-  //           name: "className",
-  //           type: "string",
-  //           default: "",
-  //           description: "Additional tailwind + CSS classes",
-  //         },
-  //       ],
-  //       // Add prop documentation for other components here
-  //     };
-
-  //     const props = propsDocs[name as keyof typeof propsDocs] || [];
-
-  //     return (
-  //       <table className="w-full border-collapse border border-gray-300 mb-4">
-  //         <thead>
-  //           <tr
-  //             className={`bg-gray-100 ${
-  //               String(theme) === "dark" ? " text-black" : ""
-  //             }`}
-  //           >
-  //             <th className="border border-gray-300 p-2">Prop</th>
-  //             <th className="border border-gray-300 p-2">Type</th>
-  //             <th className="border border-gray-300 p-2">Default</th>
-  //             <th className="border border-gray-300 p-2">Description</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>
-  //           {props.map((prop, index) => (
-  //             <tr key={index}>
-  //               <td className="border border-gray-300 p-2 font-minecraft-bold">
-  //                 {prop.name}
-  //               </td>
-  //               <td className="border border-gray-300 p-2">{prop.type}</td>
-  //               <td className="border border-gray-300 p-2">{prop.default}</td>
-  //               <td className="border border-gray-300 p-2">
-  //                 {prop.description}
-  //               </td>
-  //             </tr>
-  //           ))}
-  //         </tbody>
-  //       </table>
-  //     );
-  //   };
-
-  //   const getUsageExample = () => {
-  //     // This is a placeholder. You should replace this with actual usage examples for each component.
-  //     const examples = {
-  //       Button: `
-  // import { Button } from 'pixel-retroui';
-
-  // function MyComponent() {
-  //   return (
-  //     <Button
-  //       bg="#ff0000"
-  //       textColor="white"
-  //       borderColor="black"
-  //       shadow="darkred"
-  //       className="my-custom-class"
-  //     >
-  //       Click me!
-  //     </Button>
-  //   );
-  // }
-  //       `,
-  //       Card: `
-  // import { Card } from 'pixel-retroui';
-
-  // function MyComponent() {
-  //   return (
-  //     <Card
-  //       bg="#ff0000"
-  //       textColor="white"
-  //       borderColor="black"
-  //       shadow="darkred"
-  //       className="my-custom-class"
-  //     >
-  //       This is a card!!
-  //     </Card>
-  //   );
-  // }
-  //       `,
-  //       Input: `
-  // import { Input } from 'pixel-retroui';
-
-  // function MyComponent() {
-  //   return (
-  //     <Input
-  //       bg="#ff0000"
-  //       textColor="white"
-  //       borderColor="black"
-  //       placeholder="Type here..."
-  //       className="my-custom-class"
-  //     />
-  //   );
-  // }
-  //       `,
-  //       ProgressBar: `
-  // import { ProgressBar } from 'pixel-retroui';
-
-  // function MyComponent() {
-  //   return (
-  //     <ProgressBar size="sm" color="blue" className="w-full" progress={50} />
-  //   );
-  // }
-  //       `,
-  //       // Add usage examples for other components here
-  //     };
-
-  //     return examples[name as keyof typeof examples] || "";
-  //   };
-
-  //   return (
-  //     <div className="w-full">
-  //       <h2 className="text-3xl font-minecraft-bold mb-4">{name}</h2>
-
-  //       <section className="mb-8">
-  //         <h3 className="text-2xl font-minecraft-bold mb-2">Preview</h3>
-  //         <div className="border border-gray-300 p-4 rounded">
-  //           {renderComponentPreview(name, Component)}
-  //         </div>
-  //       </section>
-
-  //       <section className="mb-8">
-  //         <h3 className="text-2xl font-minecraft-bold mb-2">Props</h3>
-  //         {getPropsTable()}
-  //       </section>
-
-  //       <section className="mb-8">
-  //         <h3 className="text-2xl font-minecraft-bold mb-2">Usage Example</h3>
-  //         <CopyableCode
-  //           code={getUsageExample()}
-  //           className={`${
-  //             String(theme) === "dark" ? " text-black bg-white" : ""
-  //           }`}
-  //         />
-  //       </section>
-
-  //       <section>
-  //         <h3 className="text-2xl font-minecraft-bold mb-2">Description</h3>
-  //         <p>
-  //           {/* Add a detailed description of the component here */}
-  //           The {name} component is a customizable, pixel-art styled element
-  //           that brings a retro feel to your user interface. It&apos;s designed
-  //           to be flexible and easy to use, with various props for
-  //           customization.
-  //         </p>
-  //       </section>
-  //     </div>
-  //   );
-  // };
-
   return (
-    <div className="flex flex-col-reverse md:flex-row">
-      <div className="flex-grow overflow-y-auto">
-        <AnimatePresence>
-          <motion.div
-            key="grid"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-4"
-          >
-            {components.map(({ name, description, Component }) => (
-              <motion.div
-                key={name}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                onClick={() => {
-                  setSelectedComponent(name);
-                  router.push(`/${name}`);
-                }}
+    <div className="container mx-auto px-4 py-8">
+      <AnimatePresence>
+        <motion.div
+          key="grid"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-12 auto-rows-fr"
+        >
+          {components.map(({ name, description, Component }) => (
+            <motion.div
+              key={name}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={() => {
+                setSelectedComponent(name);
+                router.push(`/${name}`);
+              }}
+              className="h-full"
+            >
+              <Card
+                className="border-4 p-4 cursor-pointer transition-colors duration-200 flex flex-col h-full"
+                bg={colors.cardBg}
+                textColor={colors.text}
+                borderColor={colors.border}
+                shadowColor={colors.shadow}
               >
-                <Card
-                  className="border-4 p-4 cursor-pointer transition-colors duration-200"
-                  bg={colors.cardBg}
-                  textColor={colors.text}
-                  borderColor={colors.border}
-                  shadowColor={colors.shadow}
-                >
-                  <div className="h-36 flex items-center justify-center mb-2">
-                    {renderComponentPreview(name, Component)}
-                  </div>
-                  <h3 className="font-minecraft-bold text-xl mb-1">{name}</h3>
+                <div className="flex-grow flex items-center justify-center mb-4">
+                  {renderComponentPreview(name, Component)}
+                </div>
+                <div>
+                  <h3 className="font-minecraft-bold text-xl mb-2">{name}</h3>
                   <p className="text-sm">{description}</p>
-                </Card>
-              </motion.div>
-            ))}
-          </motion.div>
-        </AnimatePresence>
-      </div>
+                </div>
+              </Card>
+            </motion.div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,8 +1,8 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "pixel-retroui";
-import React, { useState, useEffect } from "react";
 import ComponentGrid from "./ComponentGrid";
 import { ThemeOptions, themeColors } from "../themes";
 
@@ -22,99 +22,94 @@ function Page() {
 
   return (
     <main
-      className="flex z-10 min-h-screen font-minecraft flex-col w-full gap-10 p-10 xl:p-32  bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"
+      className="flex z-10 min-h-screen font-minecraft flex-col w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"
       style={{ backgroundColor: colors.pageBg, color: colors.text }}
     >
-      <button
-        className="flex w-56 top-5 gap-1 left-8 absolute"
-        onClick={() => {
-          router.push("/");
-        }}
-      >
-        <Image src="/logo.png" className="" alt="logo" width={25} height={25} />
-        <h1 className="font-minecraft-bold">RetroUI</h1>
-      </button>
-      <div
-        className="p-0 flex cursor-pointer top-5 gap-1 right-8 absolute"
-        onClick={() => {
-          window.open("https://github.com/Dksie09/RetroUI", "_blank");
-        }}
-      >
-        <div className="flex items-center justify-center gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
+      <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-10 xl:px-16 py-8">
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8">
+          <button
+            className="flex items-center gap-2 mb-4 sm:mb-0"
+            onClick={() => router.push("/")}
           >
-            <path d="M21 5L20 5 19 5 19 3 18 3 17 3 15 3 15 5 14 5 13 5 13 8 12 8 11 8 11 6 11 5 9 5 9 3 7 3 6 3 5 3 5 5 3 5 3 14 5 14 5 16 7 16 7 18 10 18 10 21 12 21 14 21 14 18 17 18 17 16 19 16 19 14 21 14z"></path>
-          </svg>
-          Star the repo
+            <Image src="/logo.png" alt="logo" width={25} height={25} />
+            <h1 className="font-minecraft-bold text-lg sm:text-xl">RetroUI</h1>
+          </button>
+          <button
+            className="flex items-center gap-2 text-sm sm:text-base"
+            onClick={() =>
+              window.open("https://github.com/Dksie09/RetroUI", "_blank")
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M21 5L20 5 19 5 19 3 18 3 17 3 15 3 15 5 14 5 13 5 13 8 12 8 11 8 11 6 11 5 9 5 9 3 7 3 6 3 5 3 5 5 3 5 3 14 5 14 5 16 7 16 7 18 10 18 10 21 12 21 14 21 14 18 17 18 17 16 19 16 19 14 21 14z"></path>
+            </svg>
+            Star the repo
+          </button>
         </div>
-      </div>
-      <div className="">
-        <h1 className="font-minecraft-bold text-5xl">Components</h1>
 
-        <p className="mt-2">
-          Each component is built to bring nostalgic charm to your modern web
-          applications.
-        </p>
-        <div className="sm:flex mt-8 justify-between">
-          <div className=" flex">
+        <div className="mb-8">
+          <h1 className="font-minecraft-bold text-3xl sm:text-4xl md:text-5xl mb-4">
+            Components
+          </h1>
+          <p className="text-sm sm:text-base">
+            Each component is built to bring nostalgic charm to your modern web
+            applications.
+          </p>
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8">
+          <div className="flex items-center mb-4 sm:mb-0">
             <img
               src="https://cdn.discordapp.com/emojis/838344630268461066.gif"
-              className="cursor-pointer"
-              onClick={() => {
-                router.push("/installation");
-              }}
-              alt="logo"
+              className="cursor-pointer w-24 h-24 sm:w-32 sm:h-32"
+              onClick={() => router.push("/installation")}
+              alt="setup"
             />
             <div
               className={`
-    nes-balloon h-16 py-1 text-center from-left 
-    ${
-      String(theme) === "dark"
-        ? "is-dark rounded-corners-dark"
-        : "rounded-corners"
-    }
-  `}
+                balloon py-1 px-2 text-center from-left ml-2
+                ${
+                  theme === "dark"
+                    ? "is-dark rounded-corners-dark"
+                    : "rounded-corners"
+                }
+              `}
             >
-              <p>
+              <p className="text-xs sm:text-sm">
                 Click me
-                <br /> to setup your project
+                <br />
+                to setup your project
               </p>
             </div>
           </div>
 
-          <div className="flex gap-2 sm:mt-0 mt-5">
-            themes:
-            <button
-              onClick={() => setTheme("pop")}
-              id="pop"
-              className="p-3 h-1 rounded-full border border-[#fefcd0] bg-[#c381b5]"
-            ></button>
-            <button
-              onClick={() => setTheme("muddy")}
-              id="muddy"
-              className="p-3 h-1 rounded-full border border-[#ddceb4] bg-[#62471f]"
-            ></button>
-            <button
-              onClick={() => setTheme("light")}
-              id="light"
-              className="p-3 h-1 rounded-full border border-black bg-[#fffefd]"
-            ></button>
-            <button
-              onClick={() => setTheme("dark")}
-              id="dark"
-              className="p-3 h-1 rounded-full border border-white bg-[#000000]"
-            ></button>
+          <div className="flex items-center gap-2">
+            <span className="text-sm sm:text-base mr-2">themes:</span>
+            {["pop", "muddy", "light", "dark"].map((themeOption) => (
+              <button
+                key={themeOption}
+                onClick={() => setTheme(themeOption as ThemeOptions)}
+                className={`w-6 h-6 rounded-full border ${
+                  themeOption === "pop"
+                    ? "border-[#fefcd0] bg-[#c381b5]"
+                    : themeOption === "muddy"
+                    ? "border-[#ddceb4] bg-[#62471f]"
+                    : themeOption === "light"
+                    ? "border-black bg-[#fffefd]"
+                    : "border-white bg-[#000000]"
+                }`}
+              ></button>
+            ))}
           </div>
         </div>
-        <div className="flex mt-10">
-          <ComponentGrid theme={theme} />
-        </div>
+
+        <ComponentGrid theme={theme} />
       </div>
     </main>
   );
