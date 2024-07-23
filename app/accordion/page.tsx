97e9be77552a7d99ebafd807d57e3b31d5be37c9
Dksie09 <dakshiegoel@gmail.com>
2024-07-23 20:46:51 +0530

    accordion code fix

--- app/accordion/page.tsx
+++ app/accordion/page.tsx
@@ -362,19 +362,28 @@ function App() {
             >
               <CopyableCode
                 code={`<Accordion>
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Section 1</AccordionTrigger>
-        <AccordionContent>Content for section 1</AccordionContent>
+      <AccordionItem 
+          value="item-1"
+          shadowColor="purple"
+          textColor="purple">
+            <AccordionTrigger>Section 1</AccordionTrigger>
+            <AccordionContent>Content for section 1</AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Section 2</AccordionTrigger>
-        <AccordionContent>Content for section 2</AccordionContent>
+      <AccordionItem 
+          value="item-2"
+          shadowColor="green"
+          textColor="green">
+            <AccordionTrigger>Section 2</AccordionTrigger>
+            <AccordionContent>Content for section 2</AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Section 3</AccordionTrigger>
-        <AccordionContent>Content for section 3</AccordionContent>
+          <AccordionItem
+          value="item-3"
+          shadowColor="blue"
+          textColor="blue">
+            <AccordionTrigger>Section 3</AccordionTrigger>
+            <AccordionContent>Content for section 3</AccordionContent>
       </AccordionItem>
-    </Accordion>`}
+</Accordion>`}
               />
               <div className="w-full flex flex-col gap-4 items-center py-20 justify-center relative">
                 <p className="absolute top-2 left-2">Preview:</p>
