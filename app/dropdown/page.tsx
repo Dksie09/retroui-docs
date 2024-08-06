7127120f3b55364d16b7b0470bd86a684372496d
Dksie09 <dakshiegoel@gmail.com>
2024-08-07 02:06:35 +0530

    dropdown

--- app/dropdown/page.tsx
+++ app/dropdown/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import React from "react";
 import Navbar from "../components/Navbar";
+import Image from "next/image";
 
 function page() {
   return (
     <div className=" font-minecraft-bold flex flex-col items-center justify-center h-screen">
       <Navbar />
-      <h1 className=" text-2xl mb-8">Under Construction</h1>
+      <h1 className=" text-2xl mb-8">Adding Soon!</h1>
       <img
-        src="https://i.pinimg.com/originals/f0/f0/d9/f0f0d932d6e39c7af5aa305cbd8da735.gif"
+        src="https://i.pinimg.com/originals/74/63/59/74635989b770a38189fff31a8ef152ea.gif"
+        // className=" h-52"
         alt="Under Construction"
       />
     </div>
